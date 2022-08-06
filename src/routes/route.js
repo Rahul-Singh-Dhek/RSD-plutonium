61e5f97f81d0a107caab4b8ab8ab7782bb6dc9fd
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-06 15:42:32 +0530

    myLateChange

--- src/routes/route.js
+++ src/routes/route.js
@@ -19,12 +19,12 @@ router.get('/test-me', function (req, res) {
 });
 
 
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
+// router.get('/test-you', function(req, res){
+//     res.send('This is the second routes implementation')
+// })
 
-router.get('/give-me-students-data',function(req, res){
+// router.get('/give-me-students-data',function(req, res){
 
-})
+// })
 module.exports = router;
 // adding this comment for no reason
