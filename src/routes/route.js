c8a4d4d916e49227317fcb3fe83456f26cc3feee
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-08 18:31:11 +0530

    MySecond ASSignmnet

--- src/routes/route.js
+++ src/routes/route.js
@@ -58,37 +58,37 @@ router.get('/films/:filmId',function(req,res){
     res.send(result||"No movie exists with this id")
 })
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     logger.welcome()
 
-    res.send('My second ever api!')
-});
+//     res.send('My second ever api!')
+// });
 
-router.get('/students', function (req, res) {
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+// router.get('/students', function (req, res) {
+//     let students = ['Sabiha', 'Neha', 'Akash']
+//     res.send(students)
+// })
 
-router.get('/student-details/:name', function (req, res) {
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+// router.get('/student-details/:name', function (req, res) {
+//     /*
+//     params is an attribute inside request that contains 
+//     dynamic values.
+//     This value comes from the request url in the form of an 
+//     object where key is the variable defined in code 
+//     and value is what is sent in the request
+//     */
 
-    let requestParams = req.params
+//     let requestParams = req.params
 
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request " + JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+//     // JSON strigify function helps to print an entire object
+//     // We can use any ways to print an object in Javascript, JSON stringify is one of them
+//     console.log("This is the request " + JSON.stringify(requestParams))
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
 
-    res.send('Dummy response')
-})
+//     res.send('Dummy response')
+// })
 
 module.exports = router;
