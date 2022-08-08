8c5b5e732a8ac3db36f9e82a3e7c4396fbe37322
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-08 20:08:49 +0530

    4 and 5

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,6 +16,23 @@ router.get('/test-me', function (req, res) {
     formatter.myLowerCase();
     formatter.myUpperCase();
     res.send('My second ever api!');
+
+    // problem 4 and Problem 5
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+console.log(lodash.chunk(months,3));
+    res.send('My second ever api!');
+const oddno=[1,3,5,7,9,11,13,15,17,19];
+console.log("Lodash.tail output is---->",lodash.tail(oddno));
+const one=[1,2,2,3]
+const two=[3,3,4,5,5]
+const three=[6,6,6,6]
+const four=[7,7,8,8,8]
+const five=[9,9,10,10]
+const result=lodash.union(one,two,three,four,five);
+console.log("Lodash.union Output is---->",result);
+const arr=[['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']];
+console.log(lodash.fromPairs(arr));
+
 });
 
 
