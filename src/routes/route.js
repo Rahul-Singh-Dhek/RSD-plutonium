cdeae82c1144a229d62ecf746ebde2dfae02c853
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-10 19:30:07 +0530

    optional problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -42,7 +42,7 @@ router.post('/players', function (req, res) {
         let N2 = players[i].name
         if (N1 == N2) {
             match = true;
-           return res.send("Player already exists");
+            return res.send("Player already exists");
         }
     }
     if (match == false) {
@@ -50,6 +50,55 @@ router.post('/players', function (req, res) {
     }
     res.send({ data: players, status: true })
 })
+
+//OPTIONAL QUESTION------------------------------------------------------------------------------------------------------------------------
+
+let booking = [{
+    'bookingNumber': 1,
+     'sportId': 'ED101',
+     'centerId': 'BH101',
+    'type': 'private',
+    'slot': '16286598000000',
+'bookedOn': '31 / 08 / 2021',
+'bookedFor': '01 / 09 / 2021'
+   }
+];
+
+router.post('/players/:playerName/bookings/:bookingId', function (req, res) {
+    let playerName = req.params.playerName;
+    let bookingID = req.params.bookingId;
+    // console.log(playerName);
+    // console.log(bookingID);
+    let NameExist = false;
+    for (let i = 0; i < players.length; i++) {
+        let Name = players[i].name;
+        if (playerName == Name) {
+            NameExist = true;
+            break;
+        }
+    }
+    if(NameExist){
+    }else{
+        res.send({Error:"Player not found"})
+    }
+    
+    let IDexists=false;   
+    for(let i=0;i<booking.length;i++){
+        let bookingNumber = booking[i].bookingNumber
+        if(bookingNumber==bookingID){
+            IDexists=true;
+            break;
+        }
+    }
+    let body=req.body;
+    if(IDexists){
+        res.send({Error:"Booking was already processed"});
+    }else{
+        booking.push(body);
+        res.send(booking);
+    }
+})
+
 
 module.exports = router;
 
