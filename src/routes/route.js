4b0d39f9851a6d98ccdd63ec595a2cbcf0d78de8
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-15 20:15:00 +0530

    1

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,15 +5,15 @@ const UserController= require("../controllers/userController")
 const bookController= require("../controllers/bookController.js")
 const customerController=require("../controllers/customerController.js")
 //------------------------------------------------Assignment/12 August-----------------------------------------------
-router.post('/createBook',bookController.createBook)
-router.get('/listBook',bookController.listBooks)
+router.post('/createBook',bookController.createBook);
+router.get('/listBook',bookController.listBooks);
 //------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------------------
 router.post('/createCustomer',customerController.createCustomer);
 router.get('/customerList',customerController.customerList);
 //-------------------------------------------------------------------------------------------------------------
-router.post("/createUser", UserController.createUser  )
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/createUser", UserController.createUser);
+router.get("/getUsersData", UserController.getUsersData);
 //-------------------------------------------------------------------------------------------------------------
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
