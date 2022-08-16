80ba8623a7a02168913b79114f70c5e89537a8d6
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-16 18:17:25 +0530

    my

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,24 +1,21 @@
 const mongoose=require("mongoose");
 
 const bookSchema=new mongoose.Schema({
-    bookName:String,
-    autherName:String,
-    //category:String,
-    //year:Number
-    tags:[String],
-    date:{
-        type:Date,
-        default:Date.now
-    },
-    isPublished:{type:Boolean,default:false},
+    bookName:{
+        type:String,
+        required:true},
     prices:{
         indianPrice:String,
-        europePrice:String
+        europeanPrice:String
     },
-    sales:{
+    year:{
         type:Number,
-        default:10
-    }
+        default:2021
+    },
+    tags:[String],
+    autherName:String,
+    totalPages:Number,
+    stockAvailable:Boolean
 },{timestamps:true});
 
 module.exports=mongoose.model('book',bookSchema);
