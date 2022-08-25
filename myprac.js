2f166446976524d5d6bdd812456acd74ed9e1c60
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-25 15:03:09 +0530

    m1

---
+++ myprac.js
@@ -0,0 +1,11 @@
+let obj={
+    name:"rahul",
+    "my-work":{
+        exp:"2",
+        sal:{ini:"12L","last-mon":100}
+    }
+}
+console.log(obj["my-work"].sal);
+obj["my-name"]="MD";
+
+console.log(obj["my-name"]);
