cf0823c74c010c97732e1a719c3b592178e1187b
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-08-30 17:59:40 +0530

    myName

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,40 +2,65 @@ const jsonwebtoken = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 //+++++++++++++++++++++++++++++++++++++++++++++++++++ Assignment Authentication-1 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  res.status(201).send({ msg: savedData });
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+  }
+  catch (error) {
+    res.status(500).send({ msg: "Servor Error", Error: error.message })
+  }
 };
 
 const loginUser = function (req, res) {
-let user=req.user
-  let token = jsonwebtoken.sign(
-    {
-      userId: user._id.toString(), 
-      email:user.emailId,
-      organisation: "FunctionUp",
-    },
-    "hbjsd&%#@ff6t36trwE$#$^&*jh"
-  );
-  res.status(200).send({ status: true, token: token });
+  try {
+    let user = req.user
+    let token = jsonwebtoken.sign(
+      {
+        userId: user._id.toString(),
+        email: user.emailId,
+        organisation: "FunctionUp",
+      },
+      "hbjsd&%#@ff6t36trwE$#$^&*jh"
+    );
+    res.status(200).send({ status: true, token: token });
+  }
+  catch (error) {
+    res.status(500).send({ msg: "Servor Error", Error: error.message })
+  }
 };
 
-const getUserData =async function (req, res) {
-  let data=await userModel.findById(req.params.userId)
- res.status(200).send({ status: true, Data: data});
+const getUserData = async function (req, res) {
+  try {
+    let data = await userModel.findById(req.params.userId)
+    res.status(200).send({ status: true, Data: data });
+  }
+  catch (error) {
+    res.status(500).send({ msg: "Servor Error", Error: error.message })
+  }
 };
 
 const updateUser = async function (req, res) {
-  let userData = req.body;
-  let userId=req.params.userId
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-  res.status(200).send({ status: true, data: updatedUser });
+  try {
+    let userData = req.body;
+    let userId = req.params.userId
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+    res.status(200).send({ status: true, data: updatedUser });
+  }
+  catch (error) {
+    res.status(500).send({ msg: "Servor Error", Error: error.message })
+  }
 };
 
-const deleteUser=async function(req,res){
-  let userId=req.params.userId
-  let delUser=await userModel.findOneAndUpdate({_id:userId},{isDeleted:true},{new:true})
-  res.send(200).send({ status: true, data: delUser })
+const deleteUser = async function (req, res) {
+  try {
+    let userId = req.params.userId
+    let delUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true })
+    res.status(200).send({ status: true, data: delUser })
+  }
+  catch (error) {
+    res.status(500).send({ msg: "Servor Error", Error: error.message })
+  }
 }
 
 module.exports.createUser = createUser;
@@ -43,4 +68,4 @@ module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
 module.exports.deleteUser = deleteUser;
- 
+
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,50 +2,65 @@ const jsonwebtoken = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const loginCheck = async function (req, res, next) {
-  let email = req.body.emailId;
-  let password = req.body.password;
+  try {
+    let email = req.body.emailId;
+    let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: email, password: password });
-  if (!user) {
-    return res.status(400).send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-  } else {
-    req.user = user
-    next()
+    let user = await userModel.findOne({ emailId: email, password: password });
+    if (!user) {
+      return res.status(400).send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
+    } else {
+      req.user = user
+      next()
+    }
+  }
+  catch (error) {
+    res.status(500).send({ mesaage: "Servor Error", Error: error.message })
   }
 }
 
 const tokenHeader = function (req, res, next) {
-  req.token = req.headers["x-Auth-token"];
-  if (!req.token) {
+  try {
     req.token = req.headers["x-auth-token"];
+    if (!req.token) {
+      return res.status(401).send({ status: false, msg: "token must be present" });
+    }
+    next();
   }
-  if (!req.token) {
-    return res.status(400).send({ status: false, msg: "token must be present" });
+  catch (error) {
+    res.status(500).send({ mesaage: "Servor Error", Error: error.message })
+
   }
-  next();
 }
 
 const tokenCheck = function (req, res, next) {
-  try{
+  try {
     let token = req.token;
-  let decodedToken = jsonwebtoken.verify(token, "hbjsd&%#@ff6t36trwE$#$^&*jh");
-  req["decodedToken"] = decodedToken;
-  next();
-  }catch(error){
-    res.status(400).send({msg:"Tokken is incorrect",Error:error})
+    let decodedToken = jsonwebtoken.verify(token, "hbjsd&%#@ff6t36trwE$#$^&*jh");
+    req["decodedToken"] = decodedToken;
+    next();
   }
+  catch (error) {
+    res.status(500).send({ messsage: "Servor Error", Error: error.message })
+  }
+
 }
 
 const userAuthorisation = function (req, res, next) {
-  let decodedToken = req.decodedToken;
-  let userId = req.params.userId;
-  if (decodedToken.userId == userId) {
-    next()
-  } else {
-    return res.status(403).send({ status: false, msg: "User does not have Permission for this Operation" })
+  try {
+    let decodedToken = req.decodedToken;
+    let userId = req.params.userId;
+    if (decodedToken.userId == userId) {
+      next()
+    } else {
+      return res.status(403).send({ status: false, msg: "User does not have Permission for this Operation" })
+    }
+  }
+  catch (error) {
+    res.status(500).send({ mesaage: "Servor Error", Error: error.message })
   }
 }
 
